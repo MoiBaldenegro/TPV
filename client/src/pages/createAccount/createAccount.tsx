d5b3b87a1e057823c672dc8a478ce3d77f31c86e
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2023-10-02 01:51:08 -0600

    agregando dto user

---
+++ client/src/pages/createAccount/createAccount.tsx
@@ -0,0 +1,12 @@
+import styles from "./createAccount.module.css"
+
+
+
+export default function createAccount (){
+
+     return (
+        <div className={styles.container}>
+
+        </div>
+     )
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/users.schema';
+// interfaces
+import { CreateUserDto } from 'src/dto/createUser.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
 
-  async create(createUser: any){
+  async create(createUser: CreateUserDto) {
     const newUser = new this.UserModel(createUser);
     return await newUser.save();
   }
