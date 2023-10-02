463550adccdbf6f6ff3746fb376cbaa45eba6bfa
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2023-10-02 01:18:31 -0600

    add sytles

--- client/src/pages/loginPage/loginPage.tsx
+++ client/src/pages/loginPage/loginPage.tsx
@@ -3,39 +3,48 @@ import styles from "./loginPage.module.css";
 // Dependencies
 import { NavLink } from "react-router-dom";
 
+// icons
+import arrow from "../../assets/loginPage/arrow.svg"
+import tomateLogo from "../../assets/loginPage/tomateLogo.svg"
+import footerRight from "../../assets/loginPage/footerImgRight.svg"
+
 
 
 export default function LoginPage (){
 
     return (
-        <div>
-            <header>
-                <div>
-                    <select name="" id=""></select>
+        <div className={styles.loginPage}>
+            <header className={styles.header}>
+                <div className={styles.lenguajeSelect}>
+                    <select name="" id="" className={styles.selectInput}>
+                        <option value="es">Español</option>
+                        <option value="en">English</option>
+                    </select>
+                    <img src={arrow} alt="" className={styles.arrowSelect} />
                 </div>
             </header>
-            <main>
-                <div>
+            <main className={styles.centerContainer}>
+                <div className={styles.logoContainer}>
                     <span>Bienvenido</span>
-                    <img src="" alt="" />
+                    <img src={tomateLogo} alt="" />
                 </div>
-                <div>
-                    <span>Iniciar sesion</span>
-                    <div>
-                        <input placeholder="correo@ejemplo.com" type="text" />
-                        <input placeholder="password" type="text" />
-                        <div>
-                            <div>
+                <div className={styles.formContainer}>
+                    <span className={styles.formTittle} >Iniciar sesion</span>
+                    <div className={styles.form}>
+                        <input placeholder="correo@ejemplo.com" type="text" className={styles.inputForm}/>
+                        <input placeholder="password" type="text" className={styles.inputForm} />
+                        <div className={styles.checkboxContainer}>
+                            <div >
                                 <input type="checkbox" />
                                 <span>Recordar sesion</span>
                             </div>
-                            <span>Olvidaste tu contraseña?</span>
-                            
+                            <span>Olvidaste tu contraseña?</span> 
                         </div>
+                        <button type="submit" className={styles.btnEntrar}>Entrar</button>
                     </div>
                 </div>
             </main>
-            <footer>
+            <footer className={styles.footer} >
                 <div>
                     <NavLink to="#avisoDePrivacidad"  id="">Aviso de Privacidad</NavLink>
                     <span>|</span>
@@ -43,7 +52,7 @@ export default function LoginPage (){
                     <span>|</span>
                     <NavLink to="#avisoDePrivacidad"  id="">Soporte tecnico</NavLink>
                 </div>
-                <img src="" alt="" />
+                <img src={footerRight} alt="tomate-copyrigth" />
             </footer>
         </div>  
         )
