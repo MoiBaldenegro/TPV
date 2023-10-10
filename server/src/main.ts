690b8acf4a99523bba52b88a36edc7b53e834a1c
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-10 16:16:52 -0600

    add CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://tomate-pos.vercel.app/', 
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
