98b9c872ae58e32a980ab07ae9e7d2052ffca3bd
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-10 16:53:50 -0600

    CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://tomate-pos.vercel.app/', 'https://tomate-ksuxm5zwf-tomatepvs-projects.vercel.app/', "http://localhost:5173/"],
+    origin: ['https://tomate-pos.vercel.app', 'https://tomate-ksuxm5zwf-tomatepvs-projects.vercel.app', "http://localhost:5173"],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
