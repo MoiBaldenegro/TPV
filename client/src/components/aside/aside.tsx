e576a7ef6489a86354bbc54b3a460af5e15112ce
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-16 12:28:45 -0600

    resizing

--- client/src/components/aside/aside.tsx
+++ client/src/components/aside/aside.tsx
@@ -73,7 +73,7 @@ export default function Aside (){
                             alt="red-line"
                             />
                         </div>
-                        <div /* ACA PODEMOS HACER LA ANIMACION DEL ASIDE DEPLOY */>
+                        <div className={styles.animationContainer}>
                             <NavLink to="catalogo/categories" className={deployItemClass} onClick={() => handleBoard("catalogo", true,  1, )} > Categorías </NavLink>
                             <NavLink to="catalogo/products&prices" className={deployItemClass} onClick={() => handleBoard("catalogo", true,  2, )}> Productos y precios </NavLink>
                             <NavLink to="catalogo/dishes" className={deployItemClass} onClick={() => handleBoard("catalogo", true,  3, )}> Complementos </NavLink>
