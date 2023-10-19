fb87d68297c4a9bd0213bdaf9779349156b4c44c
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-19 12:54:24 -0600

    moises

--- client/src/components/main/catalogo/categorias/forms/createCategory.form.tsx
+++ client/src/components/main/catalogo/categorias/forms/createCategory.form.tsx
@@ -1,6 +1,19 @@
 import styles from "./createCategories.module.css"
+// dependencies
+import { useState } from "react";
+//icons 
+import arrow from "../../assets/loginPage/arrow.svg"
+//utils
+import { toggleLenguaje } from "../../../../../utils/toggleLenguaje";
 
-function CreateCategories({ isOpen, onClose, children }) {
+interface Props{
+    isOpen: any,
+    onClose: any,
+    children: any
+}
+
+function CreateCategories({ isOpen, onClose, children } : Props) {
+    const [ lenguajeSelect, setLenguajeSelect ] = useState(false);
   if (!isOpen) return null;
 
   return (
@@ -9,9 +22,33 @@ function CreateCategories({ isOpen, onClose, children }) {
         <button className={styles.closeButton} onClick={onClose}>
           X
         </button>
-        <h1> Se renderizo! dentro del modal</h1>
         {children}
-      </div>
+            <div>
+                <div>
+                    <div>
+                        <input type="text" readOnly value="Nombre de la categoría" />
+                    </div>
+                    <div className={styles.customSelect} onClick={toggleLenguaje}>
+                        <div className={styles.selectTrigger}>
+                            <span> Español </span>
+                            <img src={arrow} alt="" className={styles.arrowSelect} />
+                        </div>
+                        <div className={lenguajeSelect? styles.options : styles.hidden}>
+                            <span className={styles.option} >English</span>
+                            <span className={styles.option}> French</span>
+                            <span className={styles.option}>摩西</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
     </div>
   );
 }
