54cbd1d16ceb4de95da0fe3e11c18a5c8af7e196
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-24 13:09:56 -0600

    origin main

--- client/src/components/main/ventas/notas/notas.tsx
+++ client/src/components/main/ventas/notas/notas.tsx
@@ -1,12 +1,42 @@
 import styles from "./notas.module.css";
-
+//icons 
+import searchIcon from "../../../../assets/public/searchIcon.svg";
+import filterIcon from "../../../../assets/public/filterIcon.svg";
 
 export default function Notas(){
-    return(
-        <div>
-            <h1>
-                NOTAS
-            </h1>
-        </div>
+    return (
+        <div className={styles.container} >
+        <section className={styles.head}>
+            <h2>Modificadores</h2>
+        </section>
+        <section className={styles.mainSection}>
+            <div className={styles.mainHead}>
+                <div className={styles.mainHeadLeft} >
+                    <span>Mostrar</span>
+                    <select name="" id="" className={styles.showSelect}>
+                        <option value="all">Todos</option>
+                        <option value="option-one">Option 1</option>
+                        <option value="optio-two">Option 2</option>
+                    </select>
+                    <span>Modificadores</span>
+                </div>
+                <div className={styles.searchContainer} >
+                    <button className={styles.categoryButton}><img src={filterIcon} alt="categories-button" /><span>Categorias</span></button>
+                    <div className={styles.searchBarTable}>
+                        <img src={searchIcon} alt="search-icon" className={styles.searchIcon}/>
+                        <input type="text" className={styles.searchBarTableInput} placeholder="Ejemplo de modificador" />
+                    </div>
+                </div> 
+            </div>
+            <div className={styles.tableContainer}>
+                <table className={styles.table}>
+                    <thead></thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            <div className={styles.tableFooter}>
+            </div>
+        </section>
+    </div>
     )
 }
