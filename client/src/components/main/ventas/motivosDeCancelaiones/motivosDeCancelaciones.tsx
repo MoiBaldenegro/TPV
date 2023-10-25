77ff4d908f5862a2b1713764f9f6df9840ee14a4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-25 15:34:11 -0600

    sdafsfg

--- client/src/components/main/ventas/motivosDeCancelaiones/motivosDeCancelaciones.tsx
+++ client/src/components/main/ventas/motivosDeCancelaiones/motivosDeCancelaciones.tsx
@@ -1,13 +1,43 @@
 import styles from "./motivosDeCancelaciones.module.css";
 
-
+//icons 
+import searchIcon from "../../../../assets/public/searchIcon.svg";
+import filterIcon from "../../../../assets/public/filterIcon.svg";
 
 export default function MotivosDeCancelacion(){
-    return(
-        <div>
-            <h1>
-                MOTIVOS DE CANCELACION
-            </h1>
-        </div>
+    return (
+        <div className={styles.container} >
+        <section className={styles.head}>
+            <h2>Cuentas</h2>
+        </section>
+        <section className={styles.mainSection}>
+            <div className={styles.mainHead}>
+                <div className={styles.mainHeadLeft} >
+                    <span>Mostrar</span>
+                    <select name="" id="" className={styles.showSelect}>
+                        <option value="all">Todos</option>
+                        <option value="option-one">Option 1</option>
+                        <option value="optio-two">Option 2</option>
+                    </select>
+                    <span>Cuentas</span>
+                </div>
+                <div className={styles.searchContainer} >
+                    <button className={styles.categoryButton}><img src={filterIcon} alt="categories-button" /><span>Categorias</span></button>
+                    <div className={styles.searchBarTable}>
+                        <img src={searchIcon} alt="search-icon" className={styles.searchIcon}/>
+                        <input type="text" className={styles.searchBarTableInput} placeholder="Cuenta # Ejemplo-00" />
+                    </div>
+                </div> 
+            </div>
+            <div className={styles.tableContainer}>
+                <table className={styles.table}>
+                    <thead></thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            <div className={styles.tableFooter}>
+            </div>
+        </section>
+    </div>
     )
 }
