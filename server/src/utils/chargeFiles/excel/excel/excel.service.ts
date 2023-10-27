5550451f21eb0d59460c70e6e895c774bf0b9fd4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-26 18:02:08 -0600

    origin mainos

--- server/src/utils/chargeFiles/excel/excel/excel.service.ts
+++ server/src/utils/chargeFiles/excel/excel/excel.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import * as xlsx from "xlsx";
 
 @Injectable()
 export class ExcelService {
 
-    async processExcel (file : any){
-        const workbook = xlsx.read(file.buffer, { type: 'buffer' });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const data = xlsx.utils.sheet_to_json(sheet);
+    async processExcel(file: any) {
+        try {
+            const workbook = xlsx.read(file.buffer, { type: 'buffer' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const data = xlsx.utils.sheet_to_json(sheet);
             return data;
-
+        } catch (error) {
+            console.error('Error al procesar el archivo Excel:', error);
+            throw new Error('Error al procesar el archivo Excel');
+        }
     }
+    
 
 }
