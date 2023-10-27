10c024f9725c5e92c4e2b14cc74ccd37be3e462c
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-26 18:12:27 -0600

    sdafgdsgasddfa

--- server/src/utils/chargeFiles/excel/excel/excel.service.ts
+++ server/src/utils/chargeFiles/excel/excel/excel.service.ts
@@ -5,17 +5,28 @@ import * as xlsx from "xlsx";
 export class ExcelService {
 
     async processExcel(file: any) {
-        const workbook = xlsx.read(file.buffer, { type: 'buffer' });
-        const sheetNames = workbook.SheetNames;
-    
-        if (sheetNames.length === 0) {
-            throw new Error('El archivo Excel no contiene ninguna hoja de trabajo.');
+        try {
+            if (!file) {
+                throw new Error('No se proporcionó ningún archivo.');
+            }
+        
+            const workbook = xlsx.read(file.buffer, { type: 'buffer' });
+            const sheetNames = workbook.SheetNames;
+        
+            if (sheetNames.length === 0) {
+                throw new Error('El archivo Excel no contiene ninguna hoja de trabajo.');
+            }
+        
+            const sheetName = sheetNames[0]; // Accede a la primera hoja
+            const sheet = workbook.Sheets[sheetName];
+            const data = xlsx.utils.sheet_to_json(sheet);
+            return data;
+            
+        } catch (error) {
+            console.error('Error al procesar el archivo Excel:', error);
+            throw new Error('Error al procesar el archivo Excel');
         }
-    
-        const sheetName = sheetNames[0]; // Accede a la primera hoja
-        const sheet = workbook.Sheets[sheetName];
-        const data = xlsx.utils.sheet_to_json(sheet);
-        return data;
+      
     }
     
 
