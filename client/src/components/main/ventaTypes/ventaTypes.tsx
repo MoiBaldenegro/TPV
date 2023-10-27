62502a39fef0add8f5eed29bc3cc0a43335d2822
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-10-27 12:26:29 -0600

    selltypessi

--- client/src/components/main/ventaTypes/ventaTypes.tsx
+++ client/src/components/main/ventaTypes/ventaTypes.tsx
@@ -1,10 +1,23 @@
 import styles from "./ventaTypes.module.css"
 
 
+
 export default function VentaTypes (){
     return (
-        <section className={styles.contenedor}>
-            <h1> Tipos de ventas </h1>
+        <div className={styles.container} >
+        <section className={styles.head}>
+            <h2>Tipos de venta</h2>
         </section>
+        <section className={styles.mainSection}>
+            <div className={styles.tableContainer}>
+                <table className={styles.table}>
+                    <thead></thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            <div className={styles.tableFooter}>
+            </div>
+        </section>
+    </div>
     )
 }
