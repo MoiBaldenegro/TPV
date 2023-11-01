454a8a279ed24fb4683c810c0598a8f3beceb98e
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-01 11:53:56 -0600

    maosidf

--- client/src/redux/actions/catalogo/categoriesActions.ts
+++ client/src/redux/actions/catalogo/categoriesActions.ts
@@ -9,25 +9,47 @@ export const searchCategories = payload => ({type: SEARCH_CATEGORIES, payload })
 // Create Categories
 export const createCategory = category => async dispatch => {
   try {
-    if (Array.isArray(category)) {
+    if(Array.isArray(category)){
       const res = await axios.post('https://tomate-server.onrender.com/categories', category);
-      if (!res.data) {
-        throw new Error("La respuesta no contiene datos");
+      if(!res.data){
+        throw new Error("Ha ocurrido algo inesperado, la respuesta no contiene datos")
       }
       alert('Archivo subido con éxito.');
     } else {
       const response = await axios.post("https://tomate-server.onrender.com/categories", category);
-      if (!response.data) {
-        throw new Error("La respuesta no contiene datos");
+      if(!response.data){
+        throw new Error("Ha ocurrido algo inesperado, la respuesta no contiene datos")
       }
-      alert('Categoría creada con éxito.');
-      return response;
-    }
+      alert('categoria creada con éxito.');
+    } 
   } catch (error) {
-    console.error('Error:', error);
-    alert('Ha ocurrido un error al procesar la solicitud.');
+    console.log(error)
   }
+  /*
+    if (Array.isArray(category)) {
+      
+       
+       
+      
+        console.error('Error al enviar los datos al servidor:', error);
+        alert('Error al enviar los datos al servidor.');
+        return;
+      }
+    } else {
+      try {
+        
+      } catch (error) {
+        alert("Error")
+        console.log(error)
+        console.error('Error general:', error);
+      }
+      
+    } */
+   
+    
+  
 };
+
 
 //Get categories
 export const getCategories = () => {
