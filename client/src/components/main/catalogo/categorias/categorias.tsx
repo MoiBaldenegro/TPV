597c59d9e13bc2ffc4614f7468941ba1d87b04dc
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-01 12:41:12 -0600

    origin main relevantando

--- client/src/components/main/catalogo/categorias/categorias.tsx
+++ client/src/components/main/catalogo/categorias/categorias.tsx
@@ -16,8 +16,8 @@ import { useModal } from "../../../../hooks/useModals";
 
 export default function Categorias() {
 
-  const createCategory = useModal();
-  const uploadCategories = useModal();
+  const createCategory = useModal("createCategory");
+  const uploadCategories = useModal("uploadCategories");
   
   ////////////////////////////////////////////////////////////////////////////////////// 
   const dispatch = useDispatch();
@@ -53,7 +53,7 @@ export default function Categorias() {
             <img src={importIcon} alt="import-icon" />
             <span>Importar categorias</span>
           </button>
-          {uploadCategories.isOpen && uploadCategories.modalName ? <UploadFiles isOpen={uploadCategories.isOpen} onClose={uploadCategories.closeModal}> 
+          {uploadCategories.isOpen && uploadCategories.modalName === "createCategory" ? <UploadFiles isOpen={uploadCategories.isOpen} onClose={uploadCategories.closeModal}> 
           </UploadFiles> :  null }
           <button className={styles.createCategories} onClick={createCategory.openModal}>
             <img src={createIcon} alt="create-icon"/>
--- client/src/hooks/useModals.ts
+++ client/src/hooks/useModals.ts
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
-export const useModal = () => {
+// Modificamos el hook para recibir un nombre o identificador
+export const useModal = (modalName: string) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const openModal = () => {
-    console.log(isOpen);
     setIsOpen(true);
   };
 
@@ -16,5 +16,6 @@ export const useModal = () => {
     isOpen,
     openModal,
     closeModal,
+    modalName,
   };
 };
