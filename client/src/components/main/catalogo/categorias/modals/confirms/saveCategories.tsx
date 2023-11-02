d1506a365a6d5236e7208d4ecf0ce84d38b3d0f8
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-02 16:20:28 -0600

    moisesbnalde

--- client/src/components/main/catalogo/categorias/modals/confirms/saveCategories.tsx
+++ client/src/components/main/catalogo/categorias/modals/confirms/saveCategories.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import styles from "./saveCategories.module.css";
 import Loader from "../../../../../loaders/loader";
 
@@ -9,15 +10,19 @@ interface Props{
 
 
 export default function SaveCategoriesModal({ isOpen, onClose, children } : Props){
+    const { loading } = useSelector( state => state.categories );
+    const { allCategories } = useSelector( state => state.categories );  
 
    
 
     if(!isOpen) return null;
-    return (
+    loading ? <Loader/> : allCategories && allCategories.length > 0 ? (
         <div className={styles.modal}>
-            <Loader/>
-        </div>
-    )
-    
+            {children}
+			<h1>Categorias guardadas</h1>
+            <button onClick={onClose}></button>
+		</div>
+    ) : null;
+	
 	
 }
