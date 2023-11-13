a837274ef017c70c654c26febdf963e8b64b328a
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-13 09:40:55 -0600

    remove products ofr the aside

--- client/src/components/aside/ventas/ventasMenu.tsx
+++ client/src/components/aside/ventas/ventasMenu.tsx
@@ -1,30 +1,73 @@
-import LinesVentasModule from "./lines";
-import styles from "./ventasMenu.module.css";
+import LinesVentasModule from './lines';
+import styles from './ventasMenu.module.css';
 // dependencies
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 interface Props {
-    main: string,
-    redLinePosition: number,
-    handleBoard: Function
+  main: string;
+  redLinePosition: number;
+  handleBoard: Function;
 }
 
-export default function VentasMenu ({main, redLinePosition, handleBoard}: Props){
-    const deployItemClass = ({ isActive }: any) => ( isActive ? styles.isActiveDeploy : styles.notActiveDeploy);
+export default function VentasMenu({
+  main,
+  redLinePosition,
+  handleBoard,
+}: Props) {
+  const deployItemClass = ({ isActive }: any) =>
+    isActive ? styles.isActiveDeploy : styles.notActiveDeploy;
 
-    return (
-        <div className={ main === "ventas" ? styles.itemsDeployContainer : styles.hidden}>
-            <LinesVentasModule redLinePosition={redLinePosition}/>
-            <div className={styles.containerDeployItemsClass}>
-                <NavLink to="ventas/bills" className={deployItemClass} onClick={() => handleBoard( "ventas", true,  1  )} >Cuentas</NavLink>
-                <NavLink to="ventas/notes" className={deployItemClass} onClick={() => handleBoard("ventas", true,  2, )}>Notas</NavLink>
-                <NavLink to="ventas/products" className={deployItemClass} onClick={() => handleBoard("ventas", true,  3, )}>Productos</NavLink>
-                <NavLink to="ventas/payments" className={deployItemClass} onClick={() => handleBoard("ventas", true,  4, )}>Pagos</NavLink>
-                <NavLink to="ventas/discounts" className={deployItemClass}  onClick={() => handleBoard("ventas", true, 5)}>Descuentos</NavLink>
-                <NavLink to="ventas/cancellations" className={deployItemClass}  onClick={() => handleBoard("ventas", true, 6)}>Cancelaciones</NavLink>
-                <NavLink to="ventas/cancellationsReasons" className={deployItemClass}  onClick={() => handleBoard("ventas", true, 7)}>Motivos de cancelación</NavLink>
-             </div> 
-        </div>
-    )
+  return (
+    <div
+      className={
+        main === 'ventas' ? styles.itemsDeployContainer : styles.hidden
+      }
+    >
+      <LinesVentasModule redLinePosition={redLinePosition} />
+      <div className={styles.containerDeployItemsClass}>
+        <NavLink
+          to="ventas/bills"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 1)}
+        >
+          Cuentas
+        </NavLink>
+        <NavLink
+          to="ventas/notes"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 2)}
+        >
+          Notas
+        </NavLink>
+        <NavLink
+          to="ventas/payments"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 3)}
+        >
+          Pagos
+        </NavLink>
+        <NavLink
+          to="ventas/discounts"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 4)}
+        >
+          Descuentos
+        </NavLink>
+        <NavLink
+          to="ventas/cancellations"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 5)}
+        >
+          Cancelaciones
+        </NavLink>
+        <NavLink
+          to="ventas/cancellationsReasons"
+          className={deployItemClass}
+          onClick={() => handleBoard('ventas', true, 6)}
+        >
+          Motivos de cancelación
+        </NavLink>
+      </div>
+    </div>
+  );
 }
-
