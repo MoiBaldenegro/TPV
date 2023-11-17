fe8d0ec5b69b58a2b0faaba521e5a623d77c4798
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-17 14:51:55 -0600

    origin main

--- server/src/schemas/catalogo/categories.schema.ts
+++ server/src/schemas/catalogo/categories.schema.ts
@@ -18,7 +18,7 @@ export class Category {
   categoryName: string;
 
   @Prop({ type: Array, default: () => [] })
-  subCategories: (CreateCategoryDto | Category)[];
+  subCategories: CreateCategoryDto[];
 
   @Prop()
   parentCategory: string | null;
@@ -27,30 +27,6 @@ export class Category {
     default: 'enabled',
   })
   status: 'disabled' | 'enabled';
-
-  fillSubCategories(depth = 1, subCategoriesCount = 2): void {
-    if (depth <= 5) {
-      for (let i = 0; i < subCategoriesCount; i++) {
-        const defaultSubCategory: CreateCategoryDto = {
-          code: `${this.code}-${i + 1}`,
-          categoryName: `Default Category ${depth + 1} - ${i + 1}`,
-          parentCategory: this.code,
-          status: 'enabled',
-          subCategories: [],
-        };
-        (this.subCategories[i] as Category).fillSubCategories(
-          depth + 1,
-          subCategoriesCount,
-        );
-      }
-    }
-  }
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
-
-// Middleware pre que se ejecuta antes de guardar un documento
-CategorySchema.pre('save', function (next) {
-  this.fillSubCategories();
-  next();
-});
