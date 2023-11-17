49c4a835c4209e74ca54641243274736b9ae04fd
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-17 12:57:37 -0600

    pruebas de backend

--- server/src/schemas/catalogo/categories.schema.ts
+++ server/src/schemas/catalogo/categories.schema.ts
@@ -17,7 +17,17 @@ export class Category {
   })
   categoryName: string;
 
-  @Prop({ type: Array, default: () => [] })
+  @Prop({
+    type: Array,
+    default: () => [],
+    validate: {
+      validator: function (value: any[]) {
+        // La validación personalizada para asegurar que siempre haya información hasta el nivel 5
+        return validateSubCategories(value, 1);
+      },
+      message: 'subCategories must have information up to level 5',
+    },
+  })
   subCategories: CreateCategoryDto[];
 
   @Prop()
@@ -29,5 +39,28 @@ export class Category {
   status: 'disabled' | 'enabled';
 }
 
+function validateSubCategories(subCategories: any[], depth: number): boolean {
+  // Validar hasta el nivel 5
+  if (depth > 5) {
+    return true;
+  }
+
+  for (const subCategory of subCategories) {
+    // Establecer valores predeterminados si faltan
+    subCategory.code = subCategory.code || `default-code-${depth}`;
+    subCategory.categoryName =
+      subCategory.categoryName || `Default Category ${depth}`;
+    subCategory.parentCategory =
+      subCategory.parentCategory || `default-parent-${depth}`;
+    subCategory.status = subCategory.status || 'enabled';
+
+    // Validar subcategorías recursivamente
+    if (!validateSubCategories(subCategory.subCategories, depth + 1)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export const CategorySchema = SchemaFactory.createForClass(Category);
-// RESTORED
