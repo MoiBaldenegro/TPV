12d6bad76996f0b971671992174d126dff05ffb5
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-28 12:17:35 -0600

    origin main

--- server/src/catalogo/categories/categories.service.ts
+++ server/src/catalogo/categories/categories.service.ts
@@ -13,7 +13,7 @@ export class CategoriesService {
   ) {}
 
   async findAll() {
-    return await this.categoryModel.find().populate('subCategories').exec();
+    return await this.categoryModel.find().populate('subCategoryOne').exec();
   }
 
   /*create(createCategory: any){
@@ -58,59 +58,3 @@ export class CategoriesService {
     return await this.categoryModel.deleteMany({}).exec();
   }
 }
-
-/*
-import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
-import { Category } from '../schemas/categories.schema';
-import { CreateCategoryDto } from 'src/dto/categories/createCategory.dto';
-import { UpdateCategoryDto } from 'src/dto/categories/updateCategory.dto';
-
-@Injectable()
-export class CategoriesService {
-    constructor(
-        @InjectModel(Category.name) private categoryModel: Model<Category>
-    ) {}
-
-    async findAll() {
-        return this.categoryModel.find().exec();
-    }
-/*
-    async create(createCategory: CreateCategoryDto) {
-        createCategory.code = await this.generateCategoryCode(createCategory.parentCategory);
-
-        const newCategory = new this.categoryModel(createCategory);
-        return await newCategory.save();
-    }
-
-    async generateCategoryCode(parentCategoryId: string | null): Promise<string> {
-        let parentCode = '';
-        let level = 1;
-
-        if (parentCategoryId) {
-            const parentCategory = await this.categoryModel.findById(parentCategoryId).exec();
-
-            if (parentCategory) {
-                parentCode = parentCategory.code;
-                level = parentCategory.subCategories.length + 1;
-            }
-        }
-
-        const formattedLevel = level.toString().padStart(3, '0');
-        return `${parentCode}${formattedLevel}`;
-    }
- */ /*
-    async findOne(id: string) {
-        return this.categoryModel.findById(id).exec();
-    }
-
-    async delete(id: string) {
-        return this.categoryModel.findByIdAndDelete(id).exec();
-    }
-
-    async update(id: string, category: UpdateCategoryDto) {
-        return this.categoryModel.findByIdAndUpdate(id, category, { new: true }).exec();
-    }
-}
-*/
