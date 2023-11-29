a43ad2c1d4ace4a82209ce60ed27639c6067cff5
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-11-29 13:20:17 -0600

    origin main

--- server/src/schemas/catalogo/subcategories/subCategoryThree.Schema.ts
+++ server/src/schemas/catalogo/subcategories/subCategoryThree.Schema.ts
@@ -19,7 +19,7 @@ export class SubCategoryThree {
   categoryName: string;
 
   @Prop({
-    type: [{ type: MongooseSchema.Types.ObjectId, ref: 'SubCategoryOne' }],
+    type: [{ type: MongooseSchema.Types.ObjectId, ref: 'SubCategoryTwo' }],
   })
   subCategories: SubCategoryFour[];
 
