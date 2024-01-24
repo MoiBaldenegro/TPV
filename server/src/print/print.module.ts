8a1a7ee9fd43d3839f4864a41dae4f7fa0ca03bd
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-24 11:55:49 -0600

    update

--- server/src/print/print.module.ts
+++ server/src/print/print.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ServiceController } from './service/service.controller';
 import { PrintController } from './print.controller';
 import { PrintService } from './print.service';
 
 @Module({
-  controllers: [ServiceController, PrintController],
-  providers: [PrintService]
+  controllers: [PrintController],
+  providers: [PrintService],
 })
 export class PrintModule {}
