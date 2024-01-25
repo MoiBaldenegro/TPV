77e215c5a4a7a3c1e18d6447d38d22e25167dbca
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-24 18:41:22 -0600

    origin main

--- server/src/print/print.controller.ts
+++ server/src/print/print.controller.ts
@@ -9,36 +9,76 @@ import {
   ThermalPrinter,
   PrinterTypes,
   CharacterSet,
-} from 'node-thermal-printer'; // Importa las clases específicas
+} from 'node-thermal-printer';
+import * as sharp from 'sharp';
 
 @Controller('print')
 export class PrintController {
   @Post('ticket')
   async imprimirTicket(@Body() data: any): Promise<string> {
-    // Ejecutar todos los comandos. Devuelve éxito o lanza un error en caso de fallo
+    const date = new Date().toDateString();
     try {
+      // Crear instancia de la impresora térmica
       const printer = new ThermalPrinter({
-        type: PrinterTypes.EPSON, // Ajusta según tu tipo de impresora
-        interface: 'tcp://192.168.1.88', // Nombre o IP de tu impresora
-        characterSet: CharacterSet.SLOVENIA, // Configura según tus necesidades
+        type: PrinterTypes.EPSON,
+        interface: 'tcp://192.168.1.88',
+        characterSet: CharacterSet.SLOVENIA,
         removeSpecialCharacters: false,
+        width: 42,
       });
-
-      // Generar contenido del ticket
-      const { items, total } = data;
       printer.alignCenter();
-      printer.print('=== Ticket de Venta ===\n');
 
-      items.forEach((item) => {
-        printer.print(`${item.name} x${item.quantity} $${item.price}\n`);
-      });
+      /* DISEÑO DE TICKET */
+      await printer.printImage('./src/assets/icon/TomateTaqueria.png');
 
-      printer.print(`Total: $${total}\n`);
+      printer.println('');
+
+      printer.alignLeft();
+      printer.println('TOMATE TAQUERIA SA DE CV');
+      printer.println('Av. Chapultepec 361-A');
+      printer.println('Col. Americana, Guadalajara, Jalisco.');
+      printer.println('Whatsapp: 333-446-5374   RFC: TTA200827EW2');
+
+      printer.println('');
+
+      printer.println(`Fecha ${date}`);
+      printer.println('Usuario: 1016 Moises Baldenegro');
+
+      printer.println('');
+
+      printer.println('Restaurante');
+      printer.leftRight('Cuenta: 000', 'Nota: 00');
+      printer.println('Folio: 000000');
+
+      const tableConfig: {
+        text: string;
+        align?: 'LEFT' | 'CENTER' | 'RIGHT';
+        width?: number;
+        cols?: number;
+        bold?: boolean;
+      }[] = [
+        { text: '000', align: 'LEFT', width: 0.075 },
+        { text: 'Nombre del product', align: 'LEFT', width: 0.45 },
+        { text: '$0,000.00', align: 'LEFT', width: 0.22 },
+        { text: '$0,000.00', align: 'LEFT', width: 0.22 },
+      ];
+
+      // Imprimir la tabla
+      printer.tableCustom(tableConfig);
+
+      // Cortar el papel
       printer.cut();
+
+      // Otros comandos de impresión según sea necesario
+
+      // ...
+
+      // Ejecutar todos los comandos
       printer.execute();
 
       return 'Ticket impreso correctamente';
     } catch (error) {
+      // Capturar y manejar cualquier error durante la impresión
       throw new HttpException(
         'Error al imprimir el ticket',
         HttpStatus.INTERNAL_SERVER_ERROR,
@@ -46,4 +86,3 @@ export class PrintController {
     }
   }
 }
-// new commit
