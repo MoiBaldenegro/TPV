aecda8bfa47ad79ed051e25f20fed2af1ff698d8
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-26 10:13:07 -0600

    origin main

--- server/src/print/print.controller.ts
+++ server/src/print/print.controller.ts
@@ -4,85 +4,185 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  NotFoundException,
 } from '@nestjs/common';
 import {
   ThermalPrinter,
   PrinterTypes,
   CharacterSet,
 } from 'node-thermal-printer';
-import * as sharp from 'sharp';
+
+import {
+  headInfoProducts,
+  productInfo,
+  restaurantDetails,
+  userInformation,
+} from './utils/format';
 
 @Controller('print')
 export class PrintController {
   @Post('ticket')
   async imprimirTicket(@Body() data: any): Promise<string> {
     const date = new Date().toDateString();
+
     try {
-      // Crear instancia de la impresora térmica
       const printer = new ThermalPrinter({
         type: PrinterTypes.EPSON,
-        interface: 'tcp://192.168.1.88',
+        interface: `tcp://${data.tcp}`,
         characterSet: CharacterSet.SLOVENIA,
         removeSpecialCharacters: false,
         width: 42,
       });
+
+      // Función para imprimir una imagen y manejar errores
+      const printImage = async (imagePath: string) => {
+        try {
+          await printer.printImage(imagePath);
+        } catch (error) {
+          console.error(`Error al imprimir imagen ${imagePath}:`, error);
+        }
+      };
+
+      // Inicio de la impresión
       printer.alignCenter();
-
-      /* DISEÑO DE TICKET */
-      await printer.printImage('./src/assets/icon/TomateTaqueria.png');
-
+      await printImage('./src/assets/icon/TomateTaqueria.png');
       printer.println('');
 
       printer.alignLeft();
-      printer.println('TOMATE TAQUERIA SA DE CV');
-      printer.println('Av. Chapultepec 361-A');
-      printer.println('Col. Americana, Guadalajara, Jalisco.');
-      printer.println('Whatsapp: 333-446-5374   RFC: TTA200827EW2');
+      printer.println(restaurantDetails[0]);
+      printer.println(restaurantDetails[1]);
+      printer.println(restaurantDetails[2]);
+      printer.println(restaurantDetails[3]);
 
       printer.println('');
 
       printer.println(`Fecha ${date}`);
-      printer.println('Usuario: 1016 Moises Baldenegro');
+      printer.println(userInformation);
 
       printer.println('');
 
-      printer.println('Restaurante');
-      printer.leftRight('Cuenta: 000', 'Nota: 00');
-      printer.println('Folio: 000000');
+      printer.alignCenter();
+      printer.leftRight('Restaurante', '             ');
 
-      const tableConfig: {
-        text: string;
-        align?: 'LEFT' | 'CENTER' | 'RIGHT';
-        width?: number;
-        cols?: number;
-        bold?: boolean;
-      }[] = [
-        { text: '000', align: 'LEFT', width: 0.075 },
-        { text: 'Nombre del product', align: 'LEFT', width: 0.45 },
-        { text: '$0,000.00', align: 'LEFT', width: 0.22 },
-        { text: '$0,000.00', align: 'LEFT', width: 0.22 },
-      ];
+      printer.alignCenter();
+      printer.bold(true);
+      printer.leftRight('Cuenta:000', 'Nota:00');
+      printer.leftRight('Folio: 000000', '');
+      printer.bold(false);
 
-      // Imprimir la tabla
-      printer.tableCustom(tableConfig);
+      printer.setTextNormal();
+
+      printer.println('');
+
+      printer.println(headInfoProducts);
+      printer.alignCenter();
+      await printImage('./src/assets/icon/dividerTicket.png');
+
+      // Simulación de productos
+      for (let i = 0; i < 10; i++) {
+        printer.println(productInfo);
+      }
+
+      printer.alignCenter();
+      await printImage('./src/assets/icon/dividerTicket.png');
+
+      printer.println('');
+
+      printer.alignLeft();
+      printer.println('11 Productos');
+
+      printer.println('');
+
+      printer.alignCenter();
+      printer.leftRight('Subtotal', '$0,000.00');
+      printer.leftRight('Subtotal', '10% -$0,000.00');
+      printer.leftRight('Subtotal', '10% -$0,000.00');
+      printer.leftRight('Subtotal', '10% -$0,000.00');
+
+      printer.println('');
+      printer.leftRight('Total por pagar', '$0,000.00');
+      printer.setTextNormal();
+      printer.alignRight();
+      printer.println('(cantidad en texto/mxn)');
+
+      printer.println('');
+
+      printer.alignCenter();
+      printer.leftRight('Propina', '$0,000.00');
+
+      printer.println('');
+
+      printer.leftRight('Efectivo', '-$0,000.00');
+      printer.leftRight('Tarjeta de débito', '-$0,000.00');
+
+      printer.println('');
+
+      printer.bold(true);
+      printer.leftRight('Cambio', '$0,000.00');
+      printer.bold(false);
+      printer.setTextNormal();
+
+      printer.alignRight();
+      printer.println('(cantidad en texto/mxn)');
+
+      printer.alignCenter();
+      printer.leftRight('Dolar', '$00.00');
+
+      printer.println('');
+
+      printer.alignCenter();
+      await printImage('./src/assets/icon/payIcon.png');
+
+      printer.println('');
+
+      printer.alignLeft();
+      printer.println('Propina no incluida');
+
+      printer.println('');
+
+      printer.println(
+        'Pago en una sola exhibición. Efectos fiscales al pago. Régimen general de ley. Personas morales.',
+      );
+
+      printer.println('');
+
+      printer.println(
+        'Si requiere factura favor de indicarle al mesero o solicitarla por whatsapp: 333-446-5374',
+      );
+
+      printer.println('');
+
+      printer.alignCenter();
+      await printImage('./src/assets/icon/dividerTicket.png');
+
+      printer.alignCenter();
+      await printImage('./src/assets/icon/footerTicket.png');
 
       // Cortar el papel
       printer.cut();
-
-      // Otros comandos de impresión según sea necesario
-
-      // ...
 
       // Ejecutar todos los comandos
       printer.execute();
 
       return 'Ticket impreso correctamente';
     } catch (error) {
-      // Capturar y manejar cualquier error durante la impresión
-      throw new HttpException(
-        'Error al imprimir el ticket',
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      // Loguear el error y manejarlo de manera adecuada
+      console.error('Error al imprimir el ticket:', error);
+
+      // Check if the error is a Printer Error
+      if (error.message === 'Printer Error') {
+        // Handle Printer Error
+        throw new HttpException(
+          'Error al imprimir el ticket: Error en la impresora',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      } else {
+        // Handle other errors
+        throw new HttpException(
+          'Error al imprimir el ticket',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
     }
   }
 }
---
+++ server/src/print/utils/format.ts
@@ -0,0 +1,11 @@
+export const headInfoProducts = 'Can Producto          Precio      Importe';
+export const productInfo = '000 Product Name     $0,000.00   $0,000.00';
+
+export const restaurantDetails = [
+  'TOMATE TAQUERIA SA DE CV',
+  'Av.Chapultepec 361-A',
+  'Col.Americana, Guadalajara, Jalisco.',
+  'Whatsapp: 333-446-5374   RFC: TTA200827EW2',
+];
+
+export const userInformation = `Usuario: 1016 Moises Baldenegro`;
