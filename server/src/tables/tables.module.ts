0c323db7429418ea83c0f8142ccd2783b3e408c4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-01-29 14:07:18 -0600

    injetmodel de mesas

--- server/src/tables/tables.module.ts
+++ server/src/tables/tables.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TablesController } from './tables.controller';
 import { TablesService } from './tables.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Table, TableSchema } from 'src/schemas/tables/tableSchema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Table.name,
+        schema: TableSchema,
+      },
+    ]),
+  ],
   controllers: [TablesController],
-  providers: [TablesService]
+  providers: [TablesService],
 })
 export class TablesModule {}
