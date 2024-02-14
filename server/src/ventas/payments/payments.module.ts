4dd99760d28029c0fe36ff2550b1ac58e082f72a
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-14 15:22:47 -0600

    inject bill schema in paymentModule

--- server/src/ventas/payments/payments.module.ts
+++ server/src/ventas/payments/payments.module.ts
@@ -4,6 +4,7 @@ import { PaymentsService } from './payments.service';
 import { InjectModel, MongooseModule } from '@nestjs/mongoose';
 import { Payment, PaymentSchema } from 'src/schemas/ventas/payment.schema';
 import { BillsService } from '../bills/bills.service';
+import { BillSchema, Bills } from 'src/schemas/ventas/bills.schema';
 
 @Module({
   imports: [
@@ -11,6 +12,10 @@ import { BillsService } from '../bills/bills.service';
       {
         name: Payment.name,
         schema: PaymentSchema,
+      },
+      {
+        name: Bills.name,
+        schema: BillSchema,
       },
     ]),
   ],
