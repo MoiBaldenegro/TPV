6ed1bc4e48fbdaa75f8812757c6cf0b30cd4d0f9
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-22 17:50:49 -0600

    update day 02/2024

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schemas/users.schema';
-import * as base64 from 'base-64';
 
 // interfaces
 import { CreateUserDto } from 'src/dto/users/createUser.dto';
