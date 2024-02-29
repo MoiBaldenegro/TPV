883cae3c53e300142d62d153d1f8aa03e061cd78
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-29 13:05:34 -0600

    more validations

--- client/src/hooks/useValidation.ts
+++ client/src/hooks/useValidation.ts
@@ -5,23 +5,47 @@ export default function useValidation() {
   const [validate, setValidate] = useState('');
 
   function validationRegisterFirst(data: any, lastName: any /* type user */) {
-    // IMPEDIR ENVIAR VACIOS
+    const soloLetras = /^[a-zA-Z]+$/;
+
+    // IMPEDIR ENVIAR VACIOS Y VERIFICAR SOLO LETRAS - Apellido paterno
     if (!lastName.first?.length) {
-      setValidate('Apellido paterno no puede estar vacio');
+      setValidate('Apellido paterno no puede estar vacío');
       return;
     }
+
+    if (!soloLetras.test(lastName.first)) {
+      setValidate('Apellido paterno debe contener solo letras');
+      return;
+    }
+
+    // IMPEDIR ENVIAR VACIOS Y VERIFICAR SOLO LETRAS - Apellido materno
     if (!lastName.second?.length) {
-      setValidate('Apellido materno no puede estar vacio');
+      setValidate('Apellido materno no puede estar vacío');
       return;
     }
+
+    if (!soloLetras.test(lastName.second)) {
+      setValidate('Apellido materno debe contener solo letras');
+      return;
+    }
+
+    // IMPEDIR ENVIAR VACIOS Y VERIFICAR SOLO LETRAS - Nombre(s)
     if (!data.name?.length) {
-      setValidate('Nombre(s) no puede estar vacio');
+      setValidate('Nombre(s) no puede estar vacío');
       return;
     }
+
+    if (!soloLetras.test(data.name)) {
+      setValidate('Nombre(s) debe contener solo letras');
+      return;
+    }
+
+    // VERIFICAR QUE LA FECHA Y EL TURNO ESTÉN PRESENTES
     if (!data.entryDate?.length) {
       setValidate('Asigna una fecha de ingreso');
       return;
     }
+
     if (!data.shift?.length) {
       setValidate('Se debe definir un turno antes de registrar');
       return;
@@ -29,25 +53,22 @@ export default function useValidation() {
 
     // MAXIMOS NUMEROS DE CARACTERES
     if (lastName.first?.length > 35) {
-      setValidate('Apellido paterno debe ser mayor a 35 caracteres');
+      setValidate('Apellido paterno debe ser menor a 35 caracteres');
+      return;
+    }
 
-      return;
-    }
     if (lastName.second?.length > 35) {
-      setValidate('Apellido materno no debe ser mayor a 35 caracteres');
+      setValidate('Apellido materno no debe ser menor a 35 caracteres');
       return;
     }
+
     if (data.name?.length > 35) {
-      setValidate('Nombre(s) debe ser mayor a 35 caracteres');
+      setValidate('Nombre(s) debe ser menor a 35 caracteres');
       return;
     }
-    /* validar la fecha no sea mayor a la de hoy */
-    /* if (data.entryDate?.length) {
-      setValidate('Asigna una fecha de ingreso');
-      return;
-    }    */
 
     setValidate('');
   }
+
   return { validationRegisterFirst, message, validate, setMessage };
 }
