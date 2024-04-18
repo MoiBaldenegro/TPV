8ddbc1db01ef84e6e6327df2f5c8b1f789c4d46d
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-04-18 13:03:29 -0600

    create To go order module

--- server/src/ventas/orders/togo-order/togo-order.controller.ts
+++ server/src/ventas/orders/togo-order/togo-order.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
+import { TogoOrderService } from './togo-order.service';
+import { createToGoOrderDto } from 'src/dto/ventas/orders/createToGoOrder.dto';
 
 @Controller('togo-order')
-export class TogoOrderController {}
+export class TogoOrderController {
+  constructor(private togoOrderService: TogoOrderService) {}
+
+  @Post()
+  async create(@Body() body: createToGoOrderDto) {
+    try {
+      const newOrder = await this.togoOrderService.create(body);
+      return newOrder;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException(`Ya existe esta orden`);
+      }
+      throw new NotFoundException(
+        `Ha ocurrido un error inepserado, mas informacion: ${error}`,
+      );
+    }
+  }
+}
--- server/src/ventas/orders/togo-order/togo-order.service.ts
+++ server/src/ventas/orders/togo-order/togo-order.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { createToGoOrderDto } from 'src/dto/ventas/orders/createToGoOrder.dto';
+import { ToGoOrder } from 'src/schemas/ventas/orders/toGoOrder.schema';
 
 @Injectable()
-export class TogoOrderService {}
+export class TogoOrderService {
+  constructor(
+    @InjectModel(ToGoOrder.name) private toGoOrderModel: Model<ToGoOrder>,
+  ) {}
+
+  async create(body: createToGoOrderDto) {
+    const newOrder = new this.toGoOrderModel(body);
+    return newOrder.save();
+  }
+}
