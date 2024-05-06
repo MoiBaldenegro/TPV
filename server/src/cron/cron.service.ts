4718849c23dd57270931e26ff405a7b4f79ec604
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-05-06 15:24:53 -0600

    origin main

--- server/src/cron/cron.service.ts
+++ server/src/cron/cron.service.ts
@@ -18,7 +18,7 @@ export class CronService {
 
   private initializeCronJobs() {
     // Ejecutar una tarea cada minuto
-    cron.schedule('23 15 * * *', async () => {
+    cron.schedule('0 0 * * *', async () => {
       console.log(new Date().toLocaleDateString());
       const newOperatingPeriod = new this.operatingPeriodModel();
       await newOperatingPeriod.save();
