da561668cb00180b6fb5ebce0f5090e463b34973
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-05-06 11:24:49 -0600

    origin main

--- server/src/cashier-session/cashier-session.module.ts
+++ server/src/cashier-session/cashier-session.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { CashierSessionController } from './cashier-session.controller';
 import { CashierSessionService } from './cashier-session.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CashierSession } from 'src/schemas/cashierSession/cashierSession';
+import {
+  CashierSession,
+  CashierSessionSchema,
+} from 'src/schemas/cashierSession/cashierSession';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {
         name: CashierSession.name,
-        schema: CashierSession,
+        schema: CashierSessionSchema,
       },
     ]),
   ],
